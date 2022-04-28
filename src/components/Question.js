da5cebf9f40ec4d80c9a09242b87b50652d7559a
Darrian Bagley <dbagle14@gmail.com>
2022-04-28 01:46:56 -0400

    Completed assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (timeRemaining > 1) {
+        setTimeRemaining((timeRemaining) => timeRemaining - 1);
+      } else {
+        onAnswered(false);
+        setTimeRemaining(10);
+      }
+    }, 1000);
+    return () => {
+      clearTimeout(interval);
+      clearInterval(interval);
+    };
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
